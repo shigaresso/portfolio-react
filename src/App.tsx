e6443835b51cc80d46cb3e33543b2fe891fa40ed
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-04-11 02:27:49 +0900

    CSS を用いて Card を 付箋風に装飾 GitHub リポジトリに飛ばすためのアイコンの制作

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import { Card } from './components/Card';
 function App() {
   return (
     <div className="App">
-      
-      <div className='grid'>
-        <Card title="ニコニコ風コメントアプリ SCSS 導入版" text="ニコニコ風にコメントを流します" />
-        <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
-        <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
-        <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
-      </div>
+      <div className='box'>
 
+        <div className='grid'>
+          <Card title="ニコニコ風コメントアプリ SCSS 導入版" text="ニコニコ風にコメントを流します" gitHubLink='https://github.com/shigaresso' />
+          <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
+          <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
+          <Card title="ニコニコ風コメントアプリ" text="ニコニコ風にコメントを流します" />
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,26 @@
-import "../Card.css";
+import "../Card.scss";
+import GitHubImage from "../GitHub-Mark-32px.png"
 
 type card = {
     title: string
     text: string
+    gitHubLink?: string
 }
 
 export const Card = (props: card) => {
     return (
         <div className="card">
-            <div className="card_body">
+            <div className="card__body">
                 <img src="" />
-                <h2 className="card_title">{props.title}</h2>
-                <p className="card_text">{props.text}</p>
+                <h2 className="card__title">{props.title}</h2>
+                <p className="card__text">{props.text}</p>
             </div>
-            <button className="card_button">詳細を見る</button>
+            <div className="card__detail">
+                <button className="card__detail__button">プロジェクトの内容</button>
+                <a href={props.gitHubLink}>
+                    <img src={GitHubImage} className="card__detail__repository" />
+                </a>
+            </div>
         </div>
     )
 }
